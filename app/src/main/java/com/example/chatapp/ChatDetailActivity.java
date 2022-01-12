d167da49e6163fe3483a6833ca74867b76e44602
divyang5 <divyangsumesara4@gmail.com>
2022-01-12 13:40:03 +0530

    Chat-app-first

---
+++ app/src/main/java/com/example/chatapp/ChatDetailActivity.java
@@ -0,0 +1,119 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.chatapp.Adapters.ChatAdapter;
+import com.example.chatapp.Models.MessageModel;
+import com.example.chatapp.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityChatDetailBinding.inflate(getLayoutInflater());
+
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        final String senderId=auth.getUid();
+        String receiveId=getIntent().getStringExtra("userId");
+        String userName=getIntent().getStringExtra("userName");
+        String profilePic=getIntent().getStringExtra("profilePic");
+
+        binding.userName.setText(userName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.ic_avtar).into(binding.profileImage);
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //finish();
+                Intent intent=new Intent(ChatDetailActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ArrayList<MessageModel> messageModels=new ArrayList<>();
+        final ChatAdapter chatAdapter=new ChatAdapter(messageModels,this,receiveId);
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        String senderRoom=senderId + receiveId;
+        String receiverRoom= receiveId + senderId ;
+
+        database.getReference().child("chats").child(senderRoom).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageModels.clear();
+                for(DataSnapshot snapshot1:snapshot.getChildren()){
+                    MessageModel model=snapshot1.getValue(MessageModel.class);
+                    model.setMessageId(snapshot1.getKey());
+                    messageModels.add(model);
+                }
+                chatAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.etMessage.getText().toString().isEmpty()){
+                    binding.etMessage.setError("Enter Message");
+                    return;
+                }
+                String message=binding.etMessage.getText().toString();
+                final MessageModel model=new MessageModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText("");
+
+
+                database.getReference().child("chats").child(senderRoom).push().setValue(model)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                database.getReference().child("chats").child(receiverRoom)
+                                        .push()
+                                        .setValue(model)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.chatapp.Adapters.FragmentsAdapter;
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+//        DatabaseReference myRef=database.getReference("message");
+//        myRef.setValue("Hello, world!");
+
+        auth=FirebaseAuth.getInstance();
+
+        binding.viewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
+        binding.tablayout.setupWithViewPager(binding.viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                Intent intent1=new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+                Intent intent=new Intent(MainActivity.this,signInActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.groupChat:
+                Intent intentt=new Intent(MainActivity.this,GroupChatActivity.class);
+                startActivity(intentt);
+                break;
+        }
+
+        return true;
+    }
+}
